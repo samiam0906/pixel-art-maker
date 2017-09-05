4c70fdde5d557573bfe52d88540c224be3c30f30
Sam Hoffman <samdhoffman@gmail.com>
2017-09-05 13:30:23 -0400

    Add Pixel Art Exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+var selectBox1 = document.querySelector('#box1');
+selectBox1.addEventListener('click', function() {
+  selectBox1.classList.toggle("red");
+})
+
+var selectBox2 = document.querySelector('#box2');
+selectBox2.addEventListener('click', function() {
+  selectBox2.classList.toggle("red");
+})
+
+var selectBox3 = document.querySelector('#box3');
+selectBox3.addEventListener('click', function() {
+  selectBox3.classList.toggle("red");
+})
+
+var selectBox4 = document.querySelector('#box4');
+selectBox4.addEventListener('click', function() {
+  selectBox4.classList.toggle("red");
+})
